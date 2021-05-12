4a7b61c9c81d1f46f5c5dabfeda58eb1442bdf2e
Damian Betebenner <dbetebenner@nciea.org>
2021-05-12 16:32:50 -0400

    First commit of a buildable package

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+`.onLoad` <-
+function(libname, pkgname) {
+}
+
+
+`.onAttach` <-
+function(libname, pkgname) {
+	if (interactive()) {
+		packageStartupMessage('SGPmatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("SGPmatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (5-12-2021). For help: >help("SGPmatrices") or visit https://centerforassessment.github.io/SGPmatrices')
+	}
+}
