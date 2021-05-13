aeef14d1fe49c65d9184b9539c62481582f317de
Damian Betebenner <dbetebenner@nciea.org>
2021-05-13 13:22:50 -0400

    Adding in a test to addBaselineMatrices to test whether matrices are in the package

--- R/addBaselineMatrices.R
+++ R/addBaselineMatrices.R
@@ -5,6 +5,9 @@ function(state,
          SGPstateData <- SGP::SGPstateData ### Needed due to assignment of values to SGPstateData
 
          matrix.label <- paste0(paste(state, "Baseline_Matrices", sep="_"), "$", paste(state, "Baseline_Matrices", year, sep="_"))
-         SGPstateData[[state]][["Baseline_splineMatrix"]][["Coefficient_Matrices"]] <- eval(parse(text=matrix.label))
-         return(SGPstateData)
+         if (is.null(SGPstateData[[state]][["Baseline_splineMatrix"]][["Coefficient_Matrices"]] <- eval(parse(text=matrix.label)))) {
+             stop(paste(matrix.label, "are not contained in the SGPmatrices package. Check supplied `state` and `year` arguments."))
+         } else {
+            return(SGPstateData)
+         }
 } ### END addBaselineMatrices
