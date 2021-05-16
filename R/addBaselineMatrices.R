c2e768c5136393d4882ad14ccd893e1290504af5
Damian Betebenner <dbetebenner@nciea.org>
2021-05-16 03:59:49 -0400

    Updating matrices and addBaselineMatrices

--- R/addBaselineMatrices.R
+++ R/addBaselineMatrices.R
@@ -5,17 +5,17 @@ function(state,
         SGPstateData <- SGP::SGPstateData ### Needed due to assignment of values to SGPstateData
 
         ### Utility function
-        getMatrixLabel <- function(state=state, year=year) {
+        getMatrixLabel <- function(state, year) {
              if (!paste(state, "Baseline_Matrices", sep="_") %in% names(SGPmatrices::SGPmatrices)) {
                  stop("\tNOTE: Baseline matrices for state:", state, "not in SGPmatrices. Contact package maintainer to have baseline matrices added to package.")
              }
              tmp.years <- names(SGPmatrices::SGPmatrices[[paste(state, "Baseline_Matrices", sep="_")]])
              if (!year %in% tmp.years) year <- tmp.years[which(year==sort(c(tmp.years, year)))-1L]
-             matrix.label <- paste0("SGPmatrices[[", state, "]][[", year, "]]")
+             matrix.label <- paste0("SGPmatrices::SGPmatrices[['", paste(state, "Baseline_Matrices", sep="_"), "']][['", year, "']]")
              message(paste0("\tNOTE: Using ", state, " baseline matrices from ", year, "."))
              return(matrix.label)
         }
 
-        SGPstateData[[state]][["Baseline_splineMatrix"]][["Coefficient_Matrices"]] <- eval(parse(text=getMatrixLabel()))
+        SGPstateData[[state]][["Baseline_splineMatrix"]][["Coefficient_Matrices"]] <- eval(parse(text=getMatrixLabel(state, year)))
         return(SGPstateData)
 } ### END addBaselineMatrices
