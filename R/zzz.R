9afabe9f94b48b05b9969db80be3e351f255b7d2
Damian Betebenner <dbetebenner@nciea.org>
2021-06-13 21:39:19 -0400

    Adding in NH SAS matrices

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage('SGPmatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("SGPmatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (6-12-2021). For help: >help("SGPmatrices") or visit https://centerforassessment.github.io/SGPmatrices')
+		packageStartupMessage('SGPmatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("SGPmatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (6-13-2021). For help: >help("SGPmatrices") or visit https://centerforassessment.github.io/SGPmatrices')
 	}
 }
