6445f5ae6f389581a9d570ba3783d5aaf57e334e
Damian Betebenner <dbetebenner@nciea.org>
2021-08-10 13:08:47 -0400

    Update to message when adding matrices and change baseline matrices year reference to 2020 for WIDA states

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage('SGPmatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("SGPmatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (8-4-2021). For help: >help("SGPmatrices") or visit https://centerforassessment.github.io/SGPmatrices')
+		packageStartupMessage('SGPmatrices ', paste(paste0(unlist(strsplit(as.character(packageVersion("SGPmatrices")), "[.]")), c(".", "-", ".", "")), collapse=""),' (8-10-2021). For help: >help("SGPmatrices") or visit https://centerforassessment.github.io/SGPmatrices')
 	}
 }
